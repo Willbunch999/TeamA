e4631502960ea68c800eb04395407cf2f7ec5b32
Will Bunch <wabunch@gmail.com>
2021-10-11 11:40:26 -0500

    Pushing Branches

---
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.jsu.mcis.cs310.tas_fa21;
+
+
+public class Badge {
+
+    
+    public void Badge(String BadgeId){
+        
+    }
+    
+}
