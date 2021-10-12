6b6cee55e94fb0fcd45eb083884d3260573ad213
Stephen <Stephen@10.80.111.254>
2021-10-11 19:40:04 -0500

    Initial Commit

--- src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -4,11 +4,104 @@
  * and open the template in the editor.
  */
 package edu.jsu.mcis.cs310.tas_fa21;
+import java.time.*;
 
 /**
  *
  * @author wabun
  */
 public class TAS {
-    
+    public class Shift{
+        private LocalTime lunchDuration;
+        private LocalTime shiftDuration;
+        
+        public Shift(){
+            
+        }
+        
+        public void setID(){
+            
+        }
+        
+        public void setDescription(){
+            
+        }
+        
+        public void setStart(){
+            
+        }
+        
+        public void setStop(){
+            
+        }
+        
+        public void setInterval(){
+            
+        }
+        
+        public void setGracePeriod(){
+            
+        }
+        
+        public void setDock(){
+            
+        }
+        
+        public void setLunchStart(){
+            
+        }
+        
+        public void setLunchStop(){
+            
+        }
+        
+        public void setLunchDeduct(){
+            
+        }
+        
+        public int getID(){
+            return id;
+        }
+        
+        public String getDescription(){
+            return description;
+        }
+        
+        public LocalTime getStart(){
+            return start;
+        }
+        
+        public LocalTime getStop(){
+            return stop;
+        }
+        
+        public int getInterval(){
+            return interval;
+        }
+        
+        public LocalTime getGracePeriod(){
+            return gracePeriod;
+        }
+        
+        public int getDock(){
+            return dock;
+        }
+        
+        public LocalTime getLunchStart(){
+            return lunchStart;
+        }
+        
+        public LocalTime getLunchStop(){
+            return lunchStop;
+        }
+        
+        public int getLunchDeduct(){
+            return lunchDeduct;
+        }
+        
+        @Override
+        public String toString(){
+            return "";
+        }
+    }
 }
