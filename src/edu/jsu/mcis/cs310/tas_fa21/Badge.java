ad4848dbf512dac5b81a6c4bdaea6981b3d975eb
Ruri <rfigueroa@stu.jsu.edu>
2021-10-18 11:39:46 -0500

    Edit and now Completed

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -3,7 +3,7 @@
     Objects of the Badge class will contain information from the database about a single employee badge.
     Objects of the Badge classes will be used to represent already-existing badges from the database.
     All the three classes should provide  the necessary setter and getter methods for all instance fields,
-and should include methods to output their state in string form, as shown in the attached unit tests.
+    and should include methods to output their state in string form, as shown in the attached unit tests.
  */
 package edu.jsu.mcis.cs310.tas_fa21;
 
@@ -16,33 +16,32 @@ public class Badge
 {
     // declaring a private variable
     private final String descript;   
-    private final String id;            
+    private final String Badgeid;            
 
-    public Badge(String description, String id) // 
+    public Badge(String description, String Badgeid) // 
     {
         // descript is a string and is assigned to description;
         descript = description;         
         // creating a constructor
-        this.id = id;
+        this.Badgeid = Badgeid;
     }
 
-    public String getDescription()     // getDescription is an object method and it's returning the description value.
+    public String getDescript() // getDescript is an object method and it's returning the description value.
     {
-        return descript;            // Returning description value.
+        return descript;        // Returning description value.
     }
 
-    public String getID() 
-    {
-        return id;
+    public String getBadgeid() {
+        return Badgeid;
     }
-    
+
     //Overriding the "toString()" method.
     @Override
     public String toString()
     {
         StringBuilder output = new StringBuilder();
         output.append("(").append(this.descript).append(")");
-        output.append("#").append(this.id).append(" ");
+        output.append("#").append(this.Badgeid).append(" ");
         return output.toString();
     }
 }
