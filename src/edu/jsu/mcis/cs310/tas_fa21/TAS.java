6cba5000dd116bf832a9126173c82997b21fd241
Stephen <stephenchristopherthompson@gmail.com>
2021-11-04 18:31:13 -0500

    Feature 4: calculateTotalMinutes method initial commit

--- src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -5,11 +5,30 @@
  */
 package edu.jsu.mcis.cs310.tas_fa21;
 import java.time.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author wabun
  */
 public class TAS {
-   
+   public static int calculateTotalMinutes(ArrayList<Punch> dailyPunchList, Shift shift){
+       
+       int totalMinutes = 0;
+       
+       for (int i = 0; i < dailyPunchList.size(); i++){
+            Punch punch = dailyPunchList.get(i);
+            PunchType punchID = punch.getPunchtypeid();
+            if (punchID == CLOCK_IN){
+                ;
+            } else if (punchID == CLOCK_OUT){
+                ;
+            } else if (punchID == TIME_OUT){
+                ;
+            } else {
+                    
+            }
+       }
+       return totalMinutes;
+   }
 }
