2ff8b2c863aaa0ed400002d5aec474120e4615f2
Colbylee-Mincey <79664933+Colbylee-Mincey@users.noreply.github.com>
2021-11-12 08:39:10 -0600

    Feature 5

--- src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -7,6 +7,9 @@ package edu.jsu.mcis.cs310.tas_fa21;
 import java.time.*;
 import static java.time.temporal.ChronoUnit.MINUTES;
 import java.util.ArrayList;
+import java.util.HashMap;
+import org.json.simple.*;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -41,4 +44,29 @@ public class TAS {
        
         return totalMinutes;
     }
+            public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("EEE" + " LL/dd/uuuu HH:mm:ss");
+        ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
+//{\"originaltimestamp\":\"SAT 08\\/11\\/2018 05:54:58\",\"badgeid\":\"F1EE0555\",\"adjustedtimestamp\":\"SAT 08\\/11\\/2018 06:00:00\",\"adjustmenttype\":\"Interval Round\",\"terminalid\":\"105\",\"id\":\"1087\",\"punchtype\":\"CLOCK IN\"}
+        for(Punch punch: dailypunchlist){
+            
+            HashMap<String, String> punchData = new HashMap<>();
+            
+            punchData.put("id", String.valueOf(punch.getId()));
+            punchData.put("badgeid", String.valueOf(punch.getBadge().getId()));
+            punchData.put("terminalid", String.valueOf(punch.getTerminalid()));
+            punchData.put("punchtype", String.valueOf(punch.getPunchtype()));
+            punchData.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
+            punchData.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp().format(format).toUpperCase()));
+            punchData.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp().format(format).toUpperCase()));
+            jsonData.add(punchData);
+            
+        }
+        
+        String json = JSONValue.toJSONString(jsonData);
+
+        System.out.println(json);
+        return json;
+    }
+}
 }
